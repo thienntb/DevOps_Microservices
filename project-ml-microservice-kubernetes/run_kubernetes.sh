603c529452de37093075fb52255113d65467c5e8
thienntb <buuthien2412@gmail.com>
2022-11-26 19:00:52 +0700

    Add result

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+export dockerpath=buuthien2412/mydevops
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run mydevops --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mydevops 8000:80
